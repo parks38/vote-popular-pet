2379063931e2de7b704f293ef9606a7ef8867934
suna.park <suna.park@kimChang.com>
2023-05-26 10:44:01 +0900

    Init: 환경 설정 추가

---
+++ src/main/java/com/project/votepopularpet/VotePopularPetApplication.java
@@ -0,0 +1,13 @@
+package com.project.votepopularpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotePopularPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotePopularPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/votepopularpet/VotePopularPetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.votepopularpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotePopularPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
