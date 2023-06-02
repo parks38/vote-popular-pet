41a398561eb87e4c5c89d8672332ff32b76a4830
suna.park <suna.park@kimChang.com>
2023-06-02 13:46:41 +0900

    add: pet base entity 수정

--- src/main/java/com/project/votepopularpet/pet/entity/util/BaseEntity.java
+++ src/main/java/com/project/votepopularpet/pet/entity/util/BaseEntity.java
@@ -4,11 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -24,13 +21,17 @@ import java.time.LocalDateTime;
 @Getter
 public abstract class BaseEntity {
 
+    @Column(name="CREATED_BY")
     private String createdBy;
 
     @CreationTimestamp
+    @Column(name="CREATE_DATE_TIME")
     private LocalDateTime createDateTime;
 
+    @Column(name="MODIFIED_BY")
     private String modifiedBy;
 
     @UpdateTimestamp
+    @Column(name="MODIFIED_DATE_TIME")
     private LocalDateTime modifedDateTime;
 }
