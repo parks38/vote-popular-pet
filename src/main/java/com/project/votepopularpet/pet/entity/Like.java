0b397f251bcb306fca5c362ada4a3bc937f56a93
parks38 <parks38@uw.edu>
2023-06-03 21:23:34 +0900

    fix: pet/like entity 생성 및 수정

--- src/main/java/com/project/votepopularpet/pet/entity/Like.java
+++ src/main/java/com/project/votepopularpet/pet/entity/Like.java
@@ -7,39 +7,24 @@ import lombok.EqualsAndHashCode;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.List;
 
-/**
- * Pet
- *
- * @author suna.park
- * @createdDate 2023-05-26.
- */
 @EqualsAndHashCode(callSuper = false)
 @Data
 @Entity
-public class Pet extends BaseEntity implements Serializable {
+public class Like extends BaseEntity implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="LIKE_ID")
+    private Long likeId;
+
     @Column(name="PET_ID")
     private Long petId;
 
-    @Column(name="NAME")
-    private String name;
+    @Column(name="Status")
+    private Boolean Status;
 
-    @Column(name="IMAGE_URL")
-    private String imageUrl;
-
-    @Column(name="TITLE")
-    private String title;
-
-    @Column(name="DESCRIPTION")
-    private String description;
-
-    @Column(name="VOTE_COUNT")
-    private Integer voteCount;
 }
